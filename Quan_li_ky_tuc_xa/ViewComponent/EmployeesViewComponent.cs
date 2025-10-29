3b491da835d56b4cc5e327f1b7cebc5d005f7f6a
Kinz3112 <hoangbangoc2005@gmail.com>
2025-10-29 22:30:55 +0700

    2

---
+++ Quan_li_ky_tuc_xa/ViewComponent/EmployeesViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quan_li_ky_tuc_xa.Models;
+namespace Quan_li_ky_tuc_xa.ViewComponents
+{
+    public class EmployeesViewComponent : ViewComponent
+    {
+        public EmployeesViewComponent() { }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("EmployeesLeftMenu");
+        }
+    }
+}
